17a9eb86cda5ac76447e43033547119f172dcbdc
Kajal-Raghav123 <kajalraghav9368@gmail.com>
2025-04-20 14:27:34 +0530

    Updates

---
+++ precache-manifest.09167446e7216e68855a1e6b600808ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b357f3fbbd842cbcfb52b8900cd8b4a1",
+    "url": "/QuickBite/index.html"
+  },
+  {
+    "revision": "9ac0ed6a6e1d8acafbf1",
+    "url": "/QuickBite/static/css/main.3ed1ce27.chunk.css"
+  },
+  {
+    "revision": "b3bb7f269da138a043cc",
+    "url": "/QuickBite/static/js/2.5c14dd04.chunk.js"
+  },
+  {
+    "revision": "9ac0ed6a6e1d8acafbf1",
+    "url": "/QuickBite/static/js/main.3974f05e.chunk.js"
+  },
+  {
+    "revision": "5fd61fd3f1c615f9e620",
+    "url": "/QuickBite/static/js/runtime~main.35f981b0.js"
+  }
+]);
